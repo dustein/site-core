d4b4a8c53ef35108e9ef8ea35d0c81949bd8728c
dustein <steindu@gmail.com>
2025-11-09 10:51:49 -0300

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className='text-green-700 text-2xl'>HELLO CORE</h1>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+// vite.config.js (ou .ts)
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          ['babel-plugin-react-compiler'],
+        ],
+      },
+    }),
+    tailwindcss(),
+  ],
+})
