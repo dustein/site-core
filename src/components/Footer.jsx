c12e343cab2c02e1e6b5b79ac1825e53457c99d5
dustein <steindu@gmail.com>
2025-11-28 12:23:03 -0300

    fonts trouble

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer className="p-4 text-center text-xs bg-black text-core-cinza">
-      &copy; 2024 CORE. Todos os Direitos Reservados. 28-11-2025.
+      &copy; 2024 CORE. Todos os Direitos Reservados. 28-11-2025(2).
     </footer>
   )
 }
